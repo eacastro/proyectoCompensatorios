9964a9b879287be9fadec851aeb840c868119866
Edisson C <36352484+eacastro@users.noreply.github.com>
2020-02-19 14:48:06 -0500

    Add files via upload
    Añado la clase Usuario

---
+++ Usuario.java
@@ -0,0 +1,39 @@
+public class Usuario
+{
+
+	private String primerNombre;
+	private String segundoNombre;
+	private String primerApellido;
+	private String segundoApellido;
+
+	public String obtenerPrimerNombre()
+	{
+	
+		return primerNombre;
+
+	} // Fin obtenerPrimerNombre
+
+	public String obtenerSegundoNombre()
+	{
+
+		return segundoNombre;
+
+	} // Fin obtenerSegundoNombre
+
+	public String obtenerPrimerApellido()
+	{
+
+		return primerApellido;
+
+	} // Fin obtenerPrimerApellido
+
+	public String obtenerSegundoApellido()
+	{
+
+		return segundoApellido;
+
+	} // Fin obtenerSegundoApellido
+
+
+
+} // Fin clase Usuario
